a84f26c642a90244886bfe3de69607887d92e561
kaplannp <kaplannp@whitman.edu>
2021-07-06 11:42:27 -0700

    added a hack to get the ip for the server

--- eve_server.py
+++ eve_server.py
@@ -6,5 +6,16 @@ import socket
 import ot2lib
 
 if __name__ == '__main__':
-    my_ip = socket.gethostname()
+    my_ip = hack_to_get_ip()
     ot2lib.launch_eve_server(my_ip=my_ip, barrier=None)
+
+
+def hack_to_get_ip():
+    '''
+    author @zags from stack overflow
+    courtesy of stack overflow
+    '''
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    print(s.getsockname()[0])
+    s.close()
