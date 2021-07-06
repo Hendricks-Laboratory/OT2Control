11afc8a41f2d75f1168eb75d04a7948cc4572660
kaplannp <kaplannp@whitman.edu>
2021-07-06 11:45:39 -0700

    fixed hack2

--- eve_server.py
+++ eve_server.py
@@ -12,8 +12,9 @@ def hack_to_get_ip():
     '''
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
-    print(s.getsockname()[0])
+    my_ip = s.getsockname()[0]
     s.close()
+    return my_ip
 
 if __name__ == '__main__':
     my_ip = hack_to_get_ip()
