7dad6030a74d7aba5a1569979190be56dad30349
kaplannp <kaplannp@whitman.edu>
2022-01-24 21:25:55 -0800

    adjusted ip

--- controller.py
+++ controller.py
@@ -3095,6 +3095,6 @@ class PlateReader(AbstractPlateReader):
         self._set_config_attr('ControlApp', 'DisablePlateCmds','False')
         self._set_config_attr('Configuration','SimulationMode', str(0))
 if __name__ == '__main__':
-    SERVERADDR = "10.25.9.247"
+    SERVERADDR = "10.25.17.168"
     main(SERVERADDR)
 
