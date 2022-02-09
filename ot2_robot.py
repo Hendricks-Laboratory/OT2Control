c67ce7655d59a9484f9cbc663ca59ddd24291264
kaplannp <kaplannp@whitman.edu>
2022-02-08 17:28:38 -0800

    New Tube superclass. Changed top tube mix_height
    Tube Superclass is for tube specific methods (right now the 20000uL and
    50000uL tubes mix the same way).
    mix_height for the highest mix was decreased by 4 mm (so deeper)

--- ot2_robot.py
+++ ot2_robot.py
@@ -421,7 +421,55 @@ class MultiContainer(Container):
     def MIN_HEIGHT(self):
         return self.cont.MIN_HEIGHT
 
-class Tube20000uL(Container):
+class Tube(Container):
+    '''
+    This class implements shared features of all the tubes. Right now
+    this is just the mix method for 20000 and 50000. 2000 overrides this method
+    anyways.
+    '''
+    def mix(self, pipette, mix_vol, mix_code):
+        '''
+        This method is used to mix the well. Part of the container because
+        many of it's children will override this method to mix in a special way.  
+        params:  
+            Opentrons.pipette pipette: the piptette to use for the mix
+            float mix_vol: the volume to mix with (this is almost always
+              the volume of the pipette  
+            int mix_code: integer code for what type of mix. This allows for
+              different mix behaviors within a class, though the container
+              just uses it for mix iterations, and many subclasses might
+              choose to ignore it.  
+        '''
+        NUM_HEIGHTS = 4
+        # create an array of heights with the first height just a little lower
+        # than the surface
+        # top height -4 is carefully tuned to get tip in liquid
+        top_height = max(self.height - 8, self.MIN_HEIGHT) 
+        # The next part assumes you use a 300 uL pipette
+        # it could be implemented for 20uL, but realistically, you
+        # probably won't mix a tube with a 20uL pipette
+        assert (mix_vol > 20), "Trying to mix tube with 20uL pipette"
+        # it is important that tip doesn't submerge itself, hence 55mm max depth
+        if (top_height - self.MIN_HEIGHT < 55):
+            # this is normal case.
+            bottom_height = self.MIN_HEIGHT
+        else:
+            # this is case where you need to change the height so you don't dip
+            # too deep
+            bottom_height = top_height - 55
+            print("<<eve>> warning tube is too full to mix completely")
+        heights = np.linspace(bottom_height, top_height, num=NUM_HEIGHTS)
+        for height in heights:
+            #set aspiration height
+            pipette.well_bottom_clearance.aspirate = height
+            #set dispense height to same as asp
+            pipette.well_bottom_clearance.dispense = height
+            #do the actual mix
+            for i in range(2**mix_code):
+                pipette.mix(1, mix_vol, self.get_well(), rate=100.0)
+                pipette.blow_out()
+
+class Tube20000uL(Tube):
     """
     Spcific tube with measurements taken to provide implementations of abstract methods  
     INHERITED ATTRIBUTES:  
@@ -455,48 +503,6 @@ class Tube20000uL(Container):
         height = ((self.vol - self.DEAD_VOL)/(math.pi*(diameter_15/2)**2))+height_bottom_cylinder
         self.height = height if height > height_bottom_cylinder else self.MIN_HEIGHT
 
-    def mix(self, pipette, mix_vol, mix_code):
-        '''
-        This method is used to mix the well. Part of the container because
-        many of it's children will override this method to mix in a special way.  
-        params:  
-            Opentrons.pipette pipette: the piptette to use for the mix
-            float mix_vol: the volume to mix with (this is almost always
-              the volume of the pipette  
-            int mix_code: integer code for what type of mix. This allows for
-              different mix behaviors within a class, though the container
-              just uses it for mix iterations, and many subclasses might
-              choose to ignore it.  
-        '''
-        NUM_HEIGHTS = 4
-        # create an array of heights with the first height just a little lower
-        # than the surface
-        # top height -4 is carefully tuned to get tip in liquid
-        top_height = max(self.height - 4, self.MIN_HEIGHT) 
-        # The next part assumes you use a 300 uL pipette
-        # it could be implemented for 20uL, but realistically, you
-        # probably won't mix a tube with a 20uL pipette
-        assert (mix_vol > 20), "Trying to mix 20000uL tube with 20uL pipette"
-        # it is important that tip doesn't submerge itself, hence 55mm max depth
-        if (top_height - self.MIN_HEIGHT < 55):
-            # this is normal case.
-            bottom_height = self.MIN_HEIGHT
-        else:
-            # this is case where you need to change the height so you don't dip
-            # too deep
-            bottom_height = top_height - 55
-            print("<<eve>> warning tube is too full to mix completely")
-        heights = np.linspace(bottom_height, top_height, num=NUM_HEIGHTS)
-        for height in heights:
-            #set aspiration height
-            pipette.well_bottom_clearance.aspirate = height
-            #set dispense height to same as asp
-            pipette.well_bottom_clearance.dispense = height
-            #do the actual mix
-            for i in range(2**mix_code):
-                pipette.mix(1, mix_vol, self.get_well(), rate=100.0)
-                pipette.blow_out()
-
     @property
     def disp_height(self):
         return self.height + 15 #mm
@@ -506,7 +512,7 @@ class Tube20000uL(Container):
         tip_depth = 5
         return self.height - tip_depth
             
-class Tube50000uL(Container):
+class Tube50000uL(Tube):
     """
     Spcific tube with measurements taken to provide implementations of abstract methods  
     INHERITED ATTRIBUTES:  
@@ -546,49 +552,7 @@ class Tube50000uL(Container):
         tip_depth = 5
         return self.height - tip_depth
 
-    def mix(self, pipette, mix_vol, mix_code):
-        '''
-        This method is used to mix the well. Part of the container because
-        many of it's children will override this method to mix in a special way.  
-        params:  
-            Opentrons.pipette pipette: the piptette to use for the mix
-            float mix_vol: the volume to mix with (this is almost always
-              the volume of the pipette  
-            int mix_code: integer code for what type of mix. This allows for
-              different mix behaviors within a class, though the container
-              just uses it for mix iterations, and many subclasses might
-              choose to ignore it.  
-        '''
-        NUM_HEIGHTS = 4
-        # create an array of heights with the first height just a little lower
-        # than the surface
-        # top height -4 is carefully tuned to get tip in liquid
-        top_height = max(self.height - 4, self.MIN_HEIGHT) 
-        # The next part assumes you use a 300 uL pipette
-        # it could be implemented for 20uL, but realistically, you
-        # probably won't mix a tube with a 20uL pipette
-        assert (mix_vol > 20), "Trying to mix 50000uL tube with 20uL pipette"
-        # it is important that tip doesn't submerge itself, hence 55mm max depth
-        if (top_height - self.MIN_HEIGHT < 55):
-            # this is normal case.
-            bottom_height = self.MIN_HEIGHT
-        else:
-            # this is case where you need to change the height so you don't dip
-            # too deep
-            bottom_height = top_height - 55
-            print("<<eve>> warning tube is too full to mix completely")
-        heights = np.linspace(bottom_height, top_height, num=NUM_HEIGHTS)
-        for height in heights:
-            #set aspiration height
-            pipette.well_bottom_clearance.aspirate = height
-            #set dispense height to same as asp
-            pipette.well_bottom_clearance.dispense = height
-            #do the actual mix
-            for i in range(2**mix_code):
-                pipette.mix(1, mix_vol, self.get_well(), rate=100.0)
-                pipette.blow_out()
-
-class Tube2000uL(Container):
+class Tube2000uL(Tube):
     """
     2000uL tube with measurements taken to provide implementations of abstract methods  
     INHERITED ATTRIBUTES:  
