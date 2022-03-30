d6abf65107ddcd1ef082287feb8b6bd99940b6c7
kaplannp <kaplannp@whitman.edu>
2022-03-30 12:12:06 -0700

    Added duplication

--- controller.py
+++ controller.py
@@ -116,13 +116,13 @@ def launch_auto(serveraddr, rxn_sheet_name, use_cache, simulate, no_sim, no_pr):
             "test_target_1.csv", delimiter=',', dtype=float).reshape(1,-1)
     Y_SHAPE = 1 #number of reagents to learn on
     ml_model = LinReg(model, final_spectra, y_shape=Y_SHAPE, max_iters=3,
-                scan_bounds=(540,560))
+                scan_bounds=(540,560), duplication=2)
     if not no_sim:
         auto.run_simulation(ml_model, no_pr=no_pr)
     if input('would you like to run on robot and pr? [yn] ').lower() == 'y':
         model = MultiOutputRegressor(Lasso(warm_start=True, max_iter=int(1e4)))
         ml_model = LinReg(model, final_spectra, y_shape=Y_SHAPE, max_iters=24, 
-                scan_bounds=(540,560))
+                scan_bounds=(540,560),duplication=2)
         auto.run_protocol(simulate=simulate, model=ml_model,no_pr=no_pr)
 
 
