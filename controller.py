06088f6ca29b135527df57302e5ebf6d284b72f0
Hendricks Laboratory <85696658+Hendricks-Laboratory@users.noreply.github.com>
2022-06-02 09:51:15 -0700

    Updated IP address

--- controller.py
+++ controller.py
@@ -2974,6 +2974,6 @@ class PlateReader(AbstractPlateReader):
         self._set_config_attr('ControlApp', 'DisablePlateCmds','False')
         self._set_config_attr('Configuration','SimulationMode', str(0))
 if __name__ == '__main__':
-    SERVERADDR = "169.254.44.249"
+    SERVERADDR = "10.25.13.81"
     main(SERVERADDR)
 
