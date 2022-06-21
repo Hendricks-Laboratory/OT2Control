ec8c110574b3f13ab8934010f43583103cc96e53
diqv <d@ds-MacBook-Pro.local>
2022-06-21 00:19:11 -0700

    up25

--- ml_models.py
+++ ml_models.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import Lasso
 import matplotlib.pyplot as plt
 import  pandas as pd
 import numpy as np
-
+#i
 class MLModel():
     '''
     This is the base class of any machine learning model.  
