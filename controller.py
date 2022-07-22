1549ac026ec344e4d792e8b3b95c6d5847999dd4
Grant <didwayg@whitman.edu>
2022-07-22 14:23:59 -0700

    Adjust for introduction of different reagents

--- controller.py
+++ controller.py
@@ -1172,6 +1172,7 @@ class Controller(ABC):
         self.pr.exec_macro('PlateIn')
         self.pr.run_protocol(row['scan_protocol'], row['scan_filename'], layout=well_locs)
         self.pr.exec_macro('PlateOut')
+        self.save()
 
     def _update_cached_locs(self, wellnames):
         '''
@@ -3150,7 +3151,7 @@ class ScanDataFrame():
         
         reaction = self.experiment_name
 
-        well_hist_df = pd.read_csv(os.path.join('/mnt/c/Users/science_356_lab/Robot_Files/OT2Control/Eve_Out/Logs','well_history.tsv'), sep='\t')
+        well_hist_df = pd.read_csv(os.path.join(self.eve_files_path,'well_history.tsv'), sep='\t')
 
         timess = well_hist_df.timestamp.values.tolist()
         for time in timess:
