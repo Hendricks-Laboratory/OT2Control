72f68cedbb065981961f1a1d381bf967d38c68c5
Gabe Kelly <kellygj@whitman.edu>
2022-10-31 22:42:58 -0700

    Added GradientDescent class to ml_models.py

--- ml_models.py
+++ ml_models.py
@@ -1,4 +1,4 @@
-import time #TODO delete this debugging only
+import time  # TODO delete this debugging only
 from abc import ABC
 from abc import abstractmethod
 import threading
@@ -7,18 +7,20 @@ from sklearn.linear_model import Lasso
 
 import numpy as np
 
+
 class MLModel():
-    '''
-    This is the base class of any machine learning model.  
+    """
+    This is the base class of any machine learning model.
     It provides the basic interface that is required in order to use the
-    ML model with the controller  
+    ML model with the controller
     ATTRIBUTES:
         list<int> tids: a list of thread ids that this model has (or potentially some form of a
           thread executor object
         bool quit: True indicates MLModel is ready to quit, False indicates MLModel would like
-          to keep going  
+          to keep going
         y_shape is the number of reagents you're guessing at in recipes
-    '''
+    """
+
     def __init__(self, model, max_iters=np.inf):
         self.curr_iter = 0
         self.max_iters = max_iters
@@ -30,16 +32,16 @@ class MLModel():
         self.quit = self.update_quit()
 
     def train(self, X, y):
-        '''
-        This is function is used to train the ML model.  
+        """
+        This is function is used to train the ML model.
         Internally, launches a private thread
-        This call should wait on any current training threads to complete  
+        This call should wait on any current training threads to complete
         This call should launch a training thread to retrain the model on the new data
         params:
             np.array X: shape (num_pts, num_features) the recieved data for each new well
             np.array y: shape(num_pts, n_classes) the labels to predict
-        '''
-        train_thread = threading.Thread(target=self._train, name='train thread', args=(X,y))
+        """
+        train_thread = threading.Thread(target=self._train, name='train thread', args=(X, y))
         train_thread.start()
         self.curr_iter += 1
         self.update_quit()
@@ -76,10 +78,10 @@ class MLModel():
         yet.  
         '''
         train_thread = [thread for thread in threading.enumerate() \
-                            if thread.name == 'train thread']
+                        if thread.name == 'train thread']
         if train_thread:
             train_thread = train_thread[0]
-            train_thread.join() #wait till you're done training
+            train_thread.join()  # wait till you're done training
 
     def update_quit(self):
         '''
@@ -87,7 +89,7 @@ class MLModel():
         This method will just check that you have not exceded max_iters, but should be
         extended by children to check if you've reached the target.  
         '''
-        self.quit =  self.curr_iter >= self.max_iters
+        self.quit = self.curr_iter >= self.max_iters
 
     @abstractmethod
     def generate_seed_rxns(self):
@@ -98,7 +100,8 @@ class MLModel():
             np.array: (batch_size,y.shape) 
         '''
         pass
-        
+
+
 class DummyMLModel(MLModel):
     '''
     This is the base class of any machine learning model.  
@@ -112,8 +115,9 @@ class DummyMLModel(MLModel):
         int curr_iter: formally, this is the number of times the train method has been called
         int max_iters: the number of iters to execute before quiting  
     '''
+
     def __init__(self, y_shape, max_iters=np.inf, batch_size=5):
-        super().__init__(None, max_iters) #don't have a model
+        super().__init__(None, max_iters)  # don't have a model
         self.y_shape = y_shape
         self.batch_size = batch_size
 
@@ -128,7 +132,7 @@ class DummyMLModel(MLModel):
         Postconditions:  
             The model has been trained on the new data
         '''
-        with self.model_lock: #note for dummy this is not necessary, just an example
+        with self.model_lock:  # note for dummy this is not necessary, just an example
             print('<<ML>> training!')
 
     def predict(self):
@@ -152,10 +156,12 @@ class DummyMLModel(MLModel):
             np.array: (batch_size,n_features) 
         '''
         if self.generate_seed_rxns.n_calls == 0:
-            return np.ones((self.batch_size,self.y_shape)) * 3.1415e-2
+            return np.ones((self.batch_size, self.y_shape)) * 3.1415e-2
         else:
-            return np.ones((self.batch_size,self.y_shape)) * 2 * 3.1415e-2
+            return np.ones((self.batch_size, self.y_shape)) * 2 * 3.1415e-2
+
     generate_seed_rxns.n_calls = 0
+
 
 class LinReg(MLModel):
     '''
@@ -173,12 +179,13 @@ class LinReg(MLModel):
     UNIMPLEMENTED:  
       only runs for batch size of 1  
     '''
+
     def __init__(self, model, final_spectra, y_shape, max_iters, batch_size=1,
-            scan_bounds=None, duplication=1):
-        super().__init__(model, max_iters) #don't have a model
+                 scan_bounds=None, duplication=1):
+        super().__init__(model, max_iters)  # don't have a model
         self.scan_bounds = scan_bounds
         if scan_bounds:
-            #if you only want to pay attention in bounds, predict on those vals
+            # if you only want to pay attention in bounds, predict on those vals
             self.FINAL_SPECTRA = final_spectra[:, scan_bounds[0]:scan_bounds[1]]
         else:
             self.FINAL_SPECTRA = final_spectra
@@ -187,20 +194,19 @@ class LinReg(MLModel):
         self.duplication = duplication
 
     def generate_seed_rxns(self):
-        '''
+        """
         This method is called before the model is trained to generate a batch of training
-        points  
-        returns:  
-            np.array: (batch_size,n_features) 
-        '''
+        points
+        returns:
+            np.array: (batch_size,n_features)
+        """
         upper_bound = 2.5
         lower_bound = 0.25
         recipes = np.random.rand(self.batch_size, self.y_shape) \
-                * (upper_bound - lower_bound) + lower_bound
+                  * (upper_bound - lower_bound) + lower_bound
         print("seed,", recipes)
         recipes = np.repeat(recipes, self.duplication, axis=0)
         return recipes
-
 
     def predict(self):
         '''
@@ -216,8 +222,8 @@ class LinReg(MLModel):
             y_pred = self.model.predict(self.FINAL_SPECTRA)
         print("predicted", y_pred)
         breakpoint()
-        return np.repeat(y_pred, self.duplication, axis=0);
- 
+        return np.repeat(y_pred, self.duplication, axis=0)
+
     def _train(self, X, y):
         '''
         This call should wait on any current training threads to complete  
@@ -229,17 +235,17 @@ class LinReg(MLModel):
         Postconditions:  
             The model has been trained on the new data
         '''
-        #NOTE we may get fancier in the future here and do more preprocessing
+        # NOTE we may get fancier in the future here and do more preprocessing
         if self.scan_bounds:
-            #if you only want to pay attention in bounds, train on those vals
+            # if you only want to pay attention in bounds, train on those vals
             processedX = X[:, self.scan_bounds[0]:self.scan_bounds[1]]
         else:
             processedX = X
-        #update the data with the new scans
+        # update the data with the new scans
         time.sleep(40)
         print('<<ML>> training')
         with self.model_lock:
-            if isinstance(self.X,np.ndarray):
+            if isinstance(self.X, np.ndarray):
                 self.X = np.concatenate((self.X, processedX))
                 self.y = np.concatenate((self.y, y))
             else:
@@ -252,8 +258,8 @@ class LinReg(MLModel):
 
 
 class GradientDescent(MLModel):
-    def __init__(self, model, final_spectra, y_shape, max_iters, batch_size=1,
-                     scan_bounds=None, duplication=1):
+    def __init__(self, model, final_spectra, y_shape, max_iters, learning_rate, batch_size=1,
+                 scan_bounds=None, duplication=1):
         super().__init__(model, max_iters)  # don't have a model
         self.scan_bounds = scan_bounds
 
@@ -263,12 +269,34 @@ class GradientDescent(MLModel):
         else:
             self.FINAL_SPECTRA = final_spectra
 
+        self.learning_rate = 0.01
         self.y_shape = y_shape
         self.batch_size = batch_size
         self.duplication = duplication
-
-    def generate_seed_rxns(self, num_recipies):
-
-    def _train(self, X, y):
+        self.biases = [0, 0]
 
     def predict(self):
+        pass
+
+    def _train(self, X, y):
+        costs = []
+
+        # Choose whether to trim data to within certain bounds
+        if self.scan_bounds:
+            proccessedX = X[:, self.scan_bounds[0]:self.scan_bounds[1]]
+        else:
+            processedX = X
+
+        m = self.y_shape
+        alpha = self.learning_rate
+        for _ in range(self.max_iters):
+            Y_pred = self.predict()
+
+            # Compute and record cost
+            cost = (1 / 2*m) * (np.sum(Y_pred - y)**2)
+            costs.append(cost)
+
+            # update coefficients
+            self.biases[0] = self.biases[0] - (alpha * ((1/m) * np.sum(Y_pred - y)))
+            self.biases[1] = self.biases[1] - (alpha * ((1/m) * np.sum((Y_pred - y) * X)))
+
