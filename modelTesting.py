868e448ea9f0cff8df7bda1593088754f9d27dd0
Gabe Kelly <kellygj@whitman.edu>
2023-02-21 19:49:17 -0800

    Did model analysis

--- modelTesting.py
+++ modelTesting.py
@@ -39,4 +39,65 @@ for i in range(4):
     plt.title("{} vs Lambda-max".format(names[i]))
     plt.show()
 
+# Regression model testing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+from sklearn import metrics
 
+model_data = pd.DataFrame(columns=['Model', 'Mean-Squared Error', 'Mean-Absolute Error',
+                                   'Root Mean-Squared Error'])
+
+X = df_final.iloc[:, 0:4].values
+y = df_final.iloc[:, 4].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Basic Regression
+lreg = LinearRegression()
+lreg.fit(X_train, y_train)
+y_pred = lreg.predict(X_test)
+
+model_data = model_data.append({'Model': "Multiple Regression",
+                                'Mean-Squared Error': metrics.mean_squared_error(y_test, y_pred),
+                                'Mean-Absolute Error': metrics.mean_absolute_error(y_test, y_pred),
+                                'Root Mean-Squared Error': np.sqrt(metrics.mean_squared_error(y_test, y_pred))},
+                               ignore_index=True)
+
+# Polynomial Regression
+poly_feats = PolynomialFeatures(degree=4)
+X_poly = poly_feats.fit_transform(X)
+poly_reg = LinearRegression()
+poly_reg.fit(X_poly, y)
+y_pred = poly_reg.predict(X_poly)
+
+model_data = model_data.append({'Model': "Polynomial Regression",
+                                'Mean-Squared Error': metrics.mean_squared_error(y, y_pred),
+                                'Mean-Absolute Error': metrics.mean_absolute_error(y, y_pred),
+                                'Root Mean-Squared Error': np.sqrt(metrics.mean_squared_error(y, y_pred))},
+                               ignore_index=True)
+
+# SVM Regression
+svr_reg = SVR(kernel='rbf')
+svr_reg.fit(X_train, y_train)
+y_pred = svr_reg.predict(X_test)
+
+model_data = model_data.append({'Model': "SVM Regression (RBF Kernel)",
+                                'Mean-Squared Error': metrics.mean_squared_error(y_test, y_pred),
+                                'Mean-Absolute Error': metrics.mean_absolute_error(y_test, y_pred),
+                                'Root Mean-Squared Error': np.sqrt(metrics.mean_squared_error(y_test, y_pred))},
+                               ignore_index=True)
+
+# Random Forest Regression
+rf = RandomForestRegressor(n_estimators=300, max_features='sqrt', max_depth=5, random_state=18)
+rf.fit(X_train, y_train)
+y_pred = rf.predict(X_test)
+
+model_data = model_data.append({'Model': "Random-Forest Regression (300 estimators)",
+                                'Mean-Squared Error': metrics.mean_squared_error(y_test, y_pred),
+                                'Mean-Absolute Error': metrics.mean_absolute_error(y_test, y_pred),
+                                'Root Mean-Squared Error': np.sqrt(metrics.mean_squared_error(y_test, y_pred))},
+                               ignore_index=True)
+
+model_data.head(4)
