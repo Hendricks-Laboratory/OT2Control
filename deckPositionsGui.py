ac77273fdaee5ef2ea6f3af1f2283001fa105b7f
gabepm100 <gabepm100@gmail.com>
2024-01-25 15:04:17 -0800

    Added to deckpositions and put the gui in wsl

--- deckPositionsGui.py
+++ deckPositionsGui.py
@@ -1,68 +1,99 @@
 import customtkinter
-
-customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
-
-win = customtkinter.CTk()  # create CTk window like you do with the Tk window
-win.geometry("800x800")
+import gspread
 
 
-#Create a canvas object
-c = customtkinter.CTkCanvas(win, width=600, height=600, borderwidth=0, highlightthickness=0, bg="white")
-c.place(x=100,y=50)
+def drawPipets(canvas,x,y):
+    for i in range(12):
+        for j in range(8):
+            canvas.create_oval(x+20.66+(j*20) ,y+6.66+(i*12)+(i*3),x+32.66+(j*20) ,y+18.66+(i*12)+(i*3), outline="black", fill="red",width=2)
 
 
-radius = 50 #set the arc radius
-canvas_middle = [int(c['width'])/2, int(c['height'])/2] #find the middle of the canvas
+def drawReagents(canvas,x,y):
+    canvas.create_oval(x+56.66,y+26.66,x+96.66,y+66.66, outline="black", fill="white",width=2)
 
+    canvas.create_oval(x+106.66,y+26.66,x+146.66,y+66.66, outline="black", fill="white",width=2)
 
+    canvas.create_oval(x+56.66,y+76.66,x+96.66,y+116.66, outline="black", fill="white",width=2)
+    
+    canvas.create_oval(x+106.66,y+76.66,x+146.66,y+116.66, outline="black", fill="white",width=2)
+    
+    canvas.create_oval(x+56.66,y+126.66,x+96.66,y+166.66, outline="black", fill="white",width=2)
+    
+    canvas.create_oval(x+106.66,y+126.66,x+146.66,y+166.66, outline="black", fill="white",width=2)
 
-def Create_Circle(canvas,x,y,radius,color):
-    canvas.create_arc(canvas_middle[0] - radius, canvas_middle[1] - radius, canvas_middle[0] + radius, canvas_middle[1] + radius, extent=359.9999, fill="Grey")
-
-def Create_Rectangle(canvas,x,y,width,height,fill,outline):
-    a = canvas.create_rectangle(x, y, x+width, x+height, fill=fill,outline=outline)
 
 def drawBoard(canvas):
-    canvas.create_rectangle(0, 0, 600, 600, fill="grey",outline="black")
 
-    canvas.create_rectangle(0, 0, 200, 200, fill="#7A797B",outline="black")
+    canvas.create_rectangle(0, 50, 200, 250, fill="#7A797B",outline="black")
     
-    canvas.create_rectangle(200, 0, 400, 200, fill="#7A797B",outline="black")
+    canvas.create_rectangle(200, 50, 400, 250, fill="#7A797B",outline="black")
 
-    canvas.create_rectangle(0, 200, 200, 400, fill="#7A797B",outline="black")
+    canvas.create_rectangle(0, 250, 200, 450, fill="#7A797B",outline="black")
 
-    canvas.create_rectangle(200, 200, 400, 400, fill="#7A797B",outline="black")
+    canvas.create_rectangle(200, 250, 400, 450, fill="#7A797B",outline="black")
 
-    canvas.create_rectangle(400, 0, 600, 200, fill="#7A797B",outline="black")
+    canvas.create_rectangle(400, 0, 650, 250, fill="#7A797B",outline="black")
 
-    canvas.create_rectangle(200, 400, 400, 600, fill="#7A797B",outline="black")
+    canvas.create_rectangle(200, 450, 400, 650, fill="#7A797B",outline="black")
 
-    canvas.create_rectangle(400, 200, 600, 400, fill="#7A797B",outline="black")
+    canvas.create_rectangle(400, 250, 600, 450, fill="#7A797B",outline="black")
 
-    canvas.create_rectangle(0, 400, 200, 600, fill="#7A797B",outline="black")
+    canvas.create_rectangle(0, 450, 200, 650, fill="#7A797B",outline="black")
 
-    canvas.create_rectangle(400, 400, 600, 660, fill="#7A797B",outline="black")
+    canvas.create_rectangle(400, 450, 600, 650, fill="#7A797B",outline="black")
 
-    canvas.create_text(500, 100, text="Trash", fill="white", font=('Helvetica 20 bold'))
+    canvas.create_rectangle(0, 650, 200, 850, fill="#7A797B",outline="black")
+
+    canvas.create_rectangle(400, 650, 600, 850, fill="#7A797B",outline="black")
+
+    canvas.create_rectangle(200, 650, 400, 850, fill="#7A797B",outline="black")
+
+    canvas.create_text(525, 125, text="Trash", fill="white", font=('Helvetica 20 bold'))
 
 
 class Board:
-    board=[[[],[],[]],[[],[],[]],[[],[],[]]]
+    board=[(2,[0]),(1,[0]),(-1,[0]),(0,[0]),(0,[0]),(0,[0]),(0,[0]),(1,[0]),(0,[0]),(0,[0]),(1,[0]),(0,[0])]
+    positions=[[0,50],[200,50],[0,250],[200,250],[200,450],[0,450],[400,250],[400,450],[600,50],[600,250],[600,450]]
     def __init__(self,canvas):
         """Initiates Board and calls get content on each square of the board"""
         drawBoard(canvas)
+        square=0
         for i in range(len(self.board)):
-            for j in range(len(self.board[i])):
-                self.board[i][j]=self.get_contents(i,j)
-        self.print_board()
+            if square<8:
+                # calls the get content function and depending on that it will set up the board to match the values gotten from the excell sheet
+                #self.get_contents()
+                # if that board share is set to 1 it draws the reagents and if it is 2 it draws the pipets
+                if self.board[square][0]==1:
+                    drawReagents(canvas,self.positions[square][0],self.positions[square][1])
+                elif self.board[square][0]==2:
+                    drawPipets(canvas,self.positions[square][0],self.positions[square][1])
+            square+=1
 
-
-    def get_contents(self,row,column):
+            """
+            list1 = list(self.board[i])
+            self.board[i]=self.get_contents(i)
+            tuple1 = tuple(list1)
+            self.board[i]=self.get_contents(i)
+            """
+        
+        #self.print_board()
+    def get_contents(self):
         """
         This is the function which will access different parts of the excel sheet based on which square it is looking at at that moment.
         It will then set that board place as equal to that so that we can then have a fillout function
         """
+        # using the .json in order to ask for permission to get access to the google sheet. 
+        # will need to do this again on the lab computer
+        gc = gspread.service_account("C:/Users/gabep/OneDrive/Documents/school/4th year whitman/GuiTeam/OT2Control/deck-position-gui-556e4624293c.json")
+        # Open Spreadsheet by name
+        spreadsheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/124PqDU-h_bPa-WcAnTXrShyv81nVTYWc4jws1f0IWmM/edit#gid=0")
+
+        #opens the sheet by name. sheet one is the name of the page inside the spreadsheet
+        worksheet = spreadsheet.sheet1
+        
+        # gets the values from the first row of the sheet
+        values_list = worksheet.row_values(1)
+        print(values_list)
         return -1
     
 
@@ -71,10 +102,22 @@ class Board:
             print(self.board[i])
 
 
+def Close(): 
+    win.destroy() 
 
 
-board=Board(c)
-button= customtkinter.CTkButton(win,text="Hello",width=200)
-button.place(x=300,y=700)
-win.title("Circles and Arcs")
-win.mainloop()
+def run():
+    customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+    customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+    win = customtkinter.CTk()  # create CTk window like you do with the Tk window
+    win.geometry("850x1000")
+
+    #Create a canvas object
+    c = customtkinter.CTkCanvas(win, width=650, height=850, borderwidth=0, highlightthickness=0,bg ='#242424')
+    c.place(x=100,y=50)
+    board=Board(c)
+    button= customtkinter.CTkButton(win,text="Close",width=200,command=Close)
+    button.place(x=300,y=950)
+    win.title("Deck Positions")
+    win.mainloop()
+run()
