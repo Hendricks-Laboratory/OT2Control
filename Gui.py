5e004ab2b8bfee6b545b5230ecbc2a3b48482939
halversm <halversm@whitman.edu>
2024-02-05 17:29:50 -0800

    converted tk and tkk to customtkinter

--- Gui.py
+++ Gui.py
@@ -1,14 +1,13 @@
-from tkinter import *
-from tkinter import ttk
-import tkinter as tk
+import tkinter
+import customtkinter
+from customtkinter import IntVar, CHECKBUTTON
 import subprocess
 import threading
 import os
-import customtkinter
 
 
 #Create an instance of Tkinter frame
-win= Tk()
+win= tkinter.Tk()
 
 #Set the geometry of Tkinter frame
 win.geometry("750x350")
@@ -29,7 +28,7 @@ def input1(output,sim,auto):
     #test one
     command="controller.py"
     output=execute_python_file(command,string)
-    T.insert(tk.END,output)
+    T.insert(customtkinter.CTkEnd,output)
     #real one 
     #command="python controller.py -n "+string
 
@@ -48,46 +47,47 @@ def execute_python_file(file_Name,argument):
       print(f"Error: The file does not exist.")
     
 #Initialize a Label to display the User Input
-label=Label(win, text="", font=("Courier 22 bold"))
+label = customtkinter.CTkLabel(master=win, text="", font=("Courier", 22, "bold"))
+#label=Label(win, text="", font=("Courier 22 bold"))
 label.pack()
 
 # Name Label
-l = Label(win, text = "What is the name?")
-l.config(font =("Courier", 14))
+l = customtkinter.CTkLabel(master= win, text = "What is the name?")
+l.configure(font =("Courier", 14))
 l.pack()
 
 #Create an Entry widget to accept User Input
-entry= Entry(win, width= 40)
+entry= customtkinter.CTkEntry(master=win, width= 400)
 entry.focus_set()
 entry.pack()
 
 #Sim checkbox
 
-sim = tk.IntVar()
-c2 = tk.Checkbutton(win, text='Sim?',variable=sim, onvalue=1, offvalue=0)
+sim = IntVar()
+c2 = tkinter.Checkbutton(win, text='Sim?',variable=sim, onvalue=1, offvalue=0)
 c2.pack()
 
 #Sim checkbox
-auto = tk.IntVar()
-c2 = tk.Checkbutton(win, text='Auto?',variable=auto, onvalue=1, offvalue=0)
+auto = IntVar()
+c2 = tkinter.Checkbutton(win, text='Auto?',variable=auto, onvalue=1, offvalue=0)
 c2.pack()
 output="hello"
 #Create a Button to validate Entry Widget
-ttk.Button(win, text= "Execute?",width= 20, command= lambda : [display_text(),input1(output,sim,auto)]).pack(pady=20)
+customtkinter.CTkButton(win, text= "Execute?",width= 20, command= lambda : [display_text(),input1(output,sim,auto)]).pack(pady=20)
 
-ttk.Button(win, text= "Check Deck Positions?",command=run, width=30).pack()
+customtkinter.CTkButton(win, text= "Check Deck Positions?",command=run, width=30).pack()
 
 # print("should be")
 # Create text widget and specify size.
-T = Text(win, height = 5, width = 52)
+T = customtkinter.CTkTextbox(win, height = 5, width = 52)
 
 # Create label
-l = Label(win, text = "Output")
-l.config(font =("Courier", 14))
+l = customtkinter.CTkLabel(win, text = "Output")
+l.configure(font =("Courier", 14))
 l.pack()
 
 # Create text widget and specify size.
-T = Text(win, height = 5, width = 52)
+T = customtkinter.CTkTextbox(win, height = 50, width = 400)
 T.pack()
 
 
