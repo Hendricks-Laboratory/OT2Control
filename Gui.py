00a2448fdf9fdb9e650f902bcb777fb0d1e964b2
halversm <halversm@whitman.edu>
2024-02-12 17:25:17 -0800

    changed font and padding of widgets and background of textboxes

--- Gui.py
+++ Gui.py
@@ -10,7 +10,8 @@ import os
 win= tkinter.Tk()
 #Set the geometry of Tkinter frame
 win.geometry("750x350")
-win.configure(background= '#404040')
+win.configure(background= '#303030')
+win.title("OT2Control")
 
 def display_text():
    global entry
@@ -47,51 +48,52 @@ def execute_python_file(file_Name,argument):
       print(f"Error: The file does not exist.")
     
 #Initialize a Label to display the User Input
-label = customtkinter.CTkLabel(master=win, text="", font=("Courier", 22, "bold"))
+label = customtkinter.CTkLabel(master=win, text="", font=("Inter", 22, "bold"))
 #label=Label(win, text="", font=("Courier 22 bold"))
 label.pack()
 
 # Name Label
 l = customtkinter.CTkLabel(master= win, text = "What is the name?")
-l.configure(font =("Courier", 14), text_color="white")
+l.configure(font =("Inter", 16), text_color="white")
 l.pack()
 
 #Create an Entry widget to accept User Input
 entry= customtkinter.CTkEntry(master=win, width= 400)
+entry.configure(fg_color= "#585858", text_color= "white")
 entry.focus_set()
 entry.pack()
 
 #Sim checkbox
 
 sim = IntVar()
-c2 = customtkinter.CTkCheckBox(master= win, text='Sim?',variable=sim, onvalue=1, offvalue=0, fg_color= "404040", text_color= "white", border_color = "#A7A6A6")
-c2.configure(border_width= 2)
-c2.pack(padx=20, pady=10)
+c2 = customtkinter.CTkCheckBox(master= win, text='Sim?',variable=sim, onvalue=1, offvalue=0, fg_color= "303030", text_color= "white", border_color = "#A7A6A6")
+c2.configure(border_width= 2, font= ("Inter", 12))
+c2.pack(padx=20, pady= (15, 10))
 
 #Sim checkbox
 auto = IntVar()
 c2 = customtkinter.CTkCheckBox(master= win, text='Auto?',variable=auto, onvalue=1, offvalue=0, text_color= "white", border_color = "#A7A6A6")
-c2.configure(border_width= 2)
+c2.configure(border_width= 2, font= ("Inter", 12))
 c2.pack()
 output="hello"
 #Create a Button to validate Entry Widget
-customtkinter.CTkButton(win, text= "Execute?",width= 20, command= lambda : [display_text(),input1(output,sim,auto)]).pack(pady=20)
+customtkinter.CTkButton(win, text= "Execute?",width= 20, font= ("Inter", 12) ,command= lambda : [display_text(),input1(output,sim,auto)]).pack(pady=(20, 13))
 
-customtkinter.CTkButton(win, text= "Check Deck Positions?",command=run, width=30).pack()
+customtkinter.CTkButton(win, text= "Check Deck Positions?", font= ("Inter", 12), command=run, width=30).pack(pady= (0, 17))
 
 # print("should be")
 # Create text widget and specify size.
 T = customtkinter.CTkTextbox(win, height = 5, width = 52)
 
 # Create label
-l = customtkinter.CTkLabel(win, text = "Output")
-l.configure(font =("Courier", 14))
+l = customtkinter.CTkLabel(win, text = "Output", text_color= "white")
+l.configure(font =("Inter", 14))
 l.pack()
 
 # Create text widget and specify size.
 T = customtkinter.CTkTextbox(win, height = 50, width = 400)
+T.configure(fg_color= "#585858", text_color= "white")
+T.focus_set()
 T.pack()
-
-
 
 win.mainloop()
