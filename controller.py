5a17560ba62ec3582b2dd6f9972fcff0104a5b82
didwayg <75285447+didwayg@users.noreply.github.com>
2024-02-13 21:59:42 -0800

    Typo

--- controller.py
+++ controller.py
@@ -120,7 +120,7 @@ def launch_auto(serveraddr, rxn_sheet_name, use_cache, simulate, no_sim, no_pr):
     print(auto.rxn_df)
     y_shape = auto.y_shape# number of reagents to learn on
     print("starting with y_shape:", y_shape)
-    fixed_reagents = auto.robo_params['reagent_df']
+    reagent_info = auto.robo_params['reagent_df']
     # Generate bounds for each reagent, assuming concentrations range from 0 to 1
     bounds = [{'name': f'reagent_{i+1}_conc', 'type': 'continuous', 'domain': (0, 1)} for i in range(y_shape)]
     #TODO get reagent info
