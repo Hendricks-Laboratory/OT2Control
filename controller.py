7874484315524af58a9baa5bfe03ac02b4b3b1b0
rtgrossman <rgrossman105910@gmail.com>
2026-06-11 14:37:19 -0700

    Adjust plot margins

--- controller.py
+++ controller.py
@@ -3423,7 +3423,7 @@ class AutoContr(Controller):
                 color='0.35'
             )
 
-            bottom_margin = 0.30
+            bottom_margin = 0.26
         else:
             bottom_margin = 0.14
 
